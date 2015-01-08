dc4bc461db0d477aa3b1f5a7aadea89b626e9b71
Tom Liddle <tom.liddle@bullionvault.com>
2015-01-08 10:53:23 +0000

    Increased logging, increased threads

--- src/main/scala/com/tomliddle/JettyLauncher.scala
+++ src/main/scala/com/tomliddle/JettyLauncher.scala
@@ -13,7 +13,7 @@ object JettyLauncher { // this is my entry object as specified in sbt project de
 		val port = 8080
 
 		val threadPool = new QueuedThreadPool
-		threadPool.setMaxThreads(10)
+		threadPool.setMaxThreads(30)
 		val server = new Server(threadPool)
 
 		// Extra options
@@ -24,8 +24,8 @@ object JettyLauncher { // this is my entry object as specified in sbt project de
 		// Handler Structure
 		val handlers = new HandlerCollection
 		val contexts = new ContextHandlerCollection()
-		val requestLogHandler = new RequestLogHandler
-		handlers.setHandlers(List (contexts, new DefaultHandler(), requestLogHandler).toArray)
+		//val requestLogHandler = new RequestLogHandler
+		handlers.setHandlers(List (contexts, new DefaultHandler()).toArray)
 		server.setHandler(handlers)
 
 		// Context handler
@@ -59,7 +59,7 @@ object JettyLauncher { // this is my entry object as specified in sbt project de
 		//server.setHandler(stats)
 
 		// Request log
-		val requestLog = new NCSARequestLog
+		/*val requestLog = new NCSARequestLog
 		requestLog.setFilename("yyyy_mm_dd.request.log")
 		requestLog.setFilenameDateFormat("yyyy_MM_dd")
 		requestLog.setRetainDays(90)
@@ -67,18 +67,18 @@ object JettyLauncher { // this is my entry object as specified in sbt project de
 		requestLog.setExtended(true)
 		requestLog.setLogCookies(false)
 		requestLog.setLogTimeZone("GMT")
-		requestLogHandler.setRequestLog(requestLog)
+		requestLogHandler.setRequestLog(requestLog)*/
 		//server.setHandler(requestLogHandler)
 
 		// Low resources monitor
-		val lowResourcesMonitor = new LowResourceMonitor(server)
+		/*val lowResourcesMonitor = new LowResourceMonitor(server)
 		lowResourcesMonitor.setPeriod(1000)
 		lowResourcesMonitor.setLowResourcesIdleTimeout(200)
 		lowResourcesMonitor.setMonitorThreads(true)
 		lowResourcesMonitor.setMaxConnections(0)
 		lowResourcesMonitor.setMaxMemory(0)
 		lowResourcesMonitor.setMaxLowResourcesTime(5000)
-		server.addBean(lowResourcesMonitor)
+		server.addBean(lowResourcesMonitor)*/
 
 		server.start
 		server.join
